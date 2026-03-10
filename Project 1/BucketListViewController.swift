eca6c62636c7754366e5d4932130297e8342c456
Jillian Shirai <jtshirai@hawaii.edu>
2026-03-10 12:21:14 -1000

    Started storyboard set up for tab 1

---
+++ Project 1/BucketListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BucketListViewController.swift
+//  Project 1
+//
+//  Created by Jillian Shirai on 3/10/26.
+//
+
+import UIKit
+
+class BucketListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project 1/WeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherViewController.swift
+//  Project 1
+//
+//  Created by Jillian Shirai on 3/10/26.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
