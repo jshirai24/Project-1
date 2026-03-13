570914e99677cde1cfbf26db606d8f78888e9bf2
Jillian Shirai <jtshirai@hawaii.edu>
2026-03-13 10:18:02 -1000

    Completed tab 1, starting on tab 2

--- Project 1/ViewController.swift
+++ Project 1/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+  
+    @IBOutlet weak var myNameIsLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var myMajorIsLabel: UILabel!
+    
+    @IBOutlet weak var majorLabel: UILabel!
+    
+    @IBOutlet weak var aboutMeLabel: UILabel!
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var schoolLogoImage: UIImageView!
+    
+    @IBOutlet weak var funfactsBlurb: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.navigationItem.title = "About Me"
+        
+        profileImage.image = UIImage(named: "Profile Image")
+        
+        schoolLogoImage.image = UIImage(named: "UHWO")
+        
+        myNameIsLabel.text = "My Name is:"
+        
+        myMajorIsLabel.text = "My Major is:"
+        
+        nameLabel.text = "Jillian Shirai"
+        
+        majorLabel.text = "Creative Media - Game Design"
+        aboutMeLabel.text = " Fun Facts About Me"
+        
+        funfactsBlurb.text = "I am a Creative Media student at UHWO. I have two guinea pigs named Peaches and Barley. I also like to crochet!"
+       
+        
+        
+        
+        
     }
-
+   
 
 }
 
--- Project 1/WeatherViewController.swift
+++ Project 1/WeatherViewController.swift
@@ -9,21 +9,27 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    
+    @IBOutlet weak var convertButton: UIButton!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        self.navigationItem.title = "Weather Converter"
+        convertButton.setTitle("Convert", for:.normal)
+        
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
