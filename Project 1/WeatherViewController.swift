55f5f40e889cf47b5600b4aa347de98242728543
Jillian Shirai <jtshirai@hawaii.edu>
2026-03-13 11:40:54 -1000

    Completed tab 2, basic of project completed

--- Project 1/WeatherViewController.swift
+++ Project 1/WeatherViewController.swift
@@ -14,6 +14,7 @@ class WeatherViewController: UIViewController {
     
     @IBOutlet weak var resultsLabel: UILabel!
     
+    @IBOutlet weak var numbersTextField: UITextField!
     
     
     
@@ -24,10 +25,23 @@ class WeatherViewController: UIViewController {
         // Do any additional setup after loading the view.
         self.navigationItem.title = "Weather Converter"
         convertButton.setTitle("Convert", for:.normal)
+        numbersTextField.placeholder = "Enter Degrees Fahrenheit"
+        resultsLabel.isHidden = true
+    }
+    
+    @IBAction func convertButtonPressed(_ sender: Any) {
         
+        resultsLabel.isHidden = false
         
+        let farenheit = Float((numbersTextField.text!))!
+       
+        let conversion = (farenheit - 32)/1.8
+      
+        resultsLabel.text = "Today is \(String(conversion)) degrees Celsius"
         
     }
+    
+    
     
 
     
