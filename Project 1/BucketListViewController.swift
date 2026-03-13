46265750d8fdfd4159daff0d30025aa91b81f4e5
Jillian Shirai <jtshirai@hawaii.edu>
2026-03-13 10:31:51 -1000

    Completed tab 3

--- Project 1/BucketListViewController.swift
+++ Project 1/BucketListViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class BucketListViewController: UIViewController {
+class BucketListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+ 
+    var myBucketListArray : [String] = ["Write a book", "Create a video game", "Explore all of Japan", "Meet Matt Mercer", "Become a guinea pig foster"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myBucketListArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let text = myBucketListArray[indexPath.row]
+        
+        cell.textLabel?.text = text
+        
+        return cell
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        self.navigationItem.title = "Bucket List"
+        self.navigationItem.title = "My Bucket List"
         
         
         
